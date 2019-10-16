65b428b2690f015ff65b17bd410c144bcf75ca82
Mukul Jain <jainmukul1985@gmail.com>
2019-10-16 19:09:56 -0400

    changes to controller

--- as-wire/src/main/java/com/my/gaeapp/web/ASWireNettingController.java
+++ as-wire/src/main/java/com/my/gaeapp/web/ASWireNettingController.java
@@ -1,16 +1,17 @@
 package com.my.gaeapp.web;
 
 import com.my.gaeapp.dao.QuickstartSample;
-import com.my.gaeapp.model.ASWire;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import com.my.gaeapp.model.ASWireProducts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ASWireNettingController {
@@ -19,25 +20,31 @@ public class ASWireNettingController {
     @Autowired
     private QuickstartSample quickstartSample;
 
+    @CrossOrigin
     @GetMapping("/aswire")
-    public List<ASWire> get() {
-    	logger.info(">>> aswire detail request");
-    	List<ASWire> aswireList = new ArrayList<ASWire>();
-    	
+    public List<ASWireProducts> get() {
+    	logger.info(">>> get aswire detail request");
+    	List<ASWireProducts> aswireList = new ArrayList<ASWireProducts>();
+
     	for (int i = 0; i < 5; i++) {
-    		ASWire asWire = new ASWire();
-            asWire.setName("test name");
-            asWire.setTitle("test title");
-            asWire.setDescription("this is the test description");
-            asWire.setImageUri("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAA5JREFUeNpiYOUXAQgwAABFACmHqsnDAAAAAElFTkSuQmCC");
+    		ASWireProducts asWire = new ASWireProducts();
+    		asWire.setId(i);
+    		int itemNumber = i + 1;
+            asWire.setName("test name-" + itemNumber);
+            asWire.setTitle("test title-" + itemNumber);
+            asWire.setDescription("this is the test description-" + itemNumber);
+            asWire.setImageUri("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAAC" +
+                    "Qd1PeAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAA5JREFUeNpiYOUXAQ" +
+                    "gwAABFACmHqsnDAAAAAElFTkSuQmCC");
             aswireList.add(asWire);
 		}
         
         return aswireList;
     }
 
-    @GetMapping("/data")
-    public void datastore() {
+    @GetMapping("/aswire/{id}")
+    public ASWireProducts datastore(@PathVariable String id) {
         quickstartSample.data();
+        return null;
     }
 }
